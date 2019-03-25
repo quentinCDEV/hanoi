4eabf3f4446d6c64cd307ef4f1a6423a37ecf658
Quentin Laymajoux <quentin.laymajoux@gmail.com>
2019-03-25 17:17:38 +0100

    add some tests

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -13,7 +13,10 @@ public class DisqueTest {
 		Disque grand = new Disque(3);
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+		assertEquals(1, grand.compareTo(petit));
+		assertEquals(1, grand.compareTo(moyen));
+		assertEquals(1, moyen.compareTo(petit));
+		assertEquals(0, moyen.compareTo(moyen));
 	}
 
 }
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -8,7 +8,18 @@ public class TourTest {
 
 	@Test
 	public void test() {
-
+		Tour tourvide = new Tour(3);
+		Tour tourgrand = new Tour(3);
+		Tour tourpetit = new Tour(3);
+		Disque disquepetit = new Disque(1);
+		Disque disquemoyen = new Disque(2);
+		Disque disquegrand = new Disque(3);
+		tourpetit.empiler(disquepetit);
+		tourgrand.empiler(disquegrand);
+		assertEquals(true, tourvide.empiler(disquegrand));
+		assertEquals(true, tourgrand.empiler(disquepetit));
+		assertEquals(false, tourpetit.empiler(disquemoyen));
+		assertEquals(disquegrand, tourgrand.depiler());
 	}
 
 }
